aa7356419e0c4c184cb7f94257e827bde26d7770
Emmanuel Mwaipaya <mwaipayae@gmail.com>
2024-01-12 16:36:56 +0200

    Prints arguments entered

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all arguments enterd
+ * @argc: argument counter
+ * @argv: argument array
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
