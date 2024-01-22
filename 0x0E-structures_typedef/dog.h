8621a4356b35a44d206534094e5fd20243fa9f81
Emmanuel Mwaipaya <mwaipayae@gmail.com>
2024-01-22 18:54:55 +0200

    added all funtion definitions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,5 +15,9 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
